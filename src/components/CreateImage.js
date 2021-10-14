5d3e4f043c072b45574e5591d59bcc00e6e448c0
Piyush Dubey <piyushdubey@microsoft.com>
2021-10-14 12:42:13 -0700

    Update style to use bootstrap

--- src/components/CreateImage.js
+++ src/components/CreateImage.js
@@ -2,17 +2,16 @@ import React from "react";
 import { Stack, DefaultButton } from '@fluentui/react';
 import { ReactComponent as CreateImagePic } from "./CreateImage.svg";
 import { useHistory } from "react-router-dom";
-
+import 'bootstrap/dist/css/bootstrap.css';
 import {
   mergeStyles,
   IStackItemStyles
 } from "office-ui-fabric-react";
 
 export const labelStyles = mergeStyles({
-  fontFamily: "Aharoni",
-  fontSize: "50px",
-  paddingLeft : "40%",
-  paddingBottom: "2.5%"
+  // fontFamily: "Aharoni",
+  // fontSize: "50px",
+  paddingTop : "5%",
 });
 
 export const createImageStyles = mergeStyles({
@@ -44,19 +43,22 @@ const CreateImage = () => {
     let history = useHistory();
 return (
     <Stack>
-      <h1 className={labelStyles}>Azure Picasso</h1>
-      <h6 className={subLabel1Styles}>
-        {
-"Welcome to Azure Picasso."
-        }</h6>
-        <h6 className={subLabel2Styles}>
-      {" Your portal to amplify your digital art  through Azure AI (GAN)."}</h6>
-      <div className = {buttonDivStyles}>
-    <button className={buttonStyles} onClick={()=> {history.push("mint")}}> Go Create </button>
+      <div className="container">
+        <h1 className="text-center" style={{ paddingTop: "10%"}}>Azure Picasso</h1>
+        <h3 className="text-center" style={{ paddingTop: "2%"}}>{"Welcome to Azure Picasso."}</h3>
+        <h4 className="text-center">{" Your portal to create NFTs with art powered by Azure AI"}</h4>
+        
+        <div className = "row" style={{ paddingTop: "3%"}}>
+          <div className = "col text-center">
+            <button className="btn btn-primary btn-lg" onClick={()=> {history.push("mint")}}> Start </button>
+          </div>
+        </div>
+        <div className="row" style={{ paddingTop: "4%"}}>
+          <div className="col text-center">
+          <CreateImagePic/>
+          </div>
+        </div>
     </div>
-    <div className={createImageStyles}>
-    <CreateImagePic/>
-  </div>
   </Stack>
 );
 }
